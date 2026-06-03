22408e54862abed9341b24b46273dc0bd3328081
gandalf <94477022+MamuzaD@users.noreply.github.com>
2026-06-03 14:47:28 -0700

    feat: mac install script

---
+++ install/mac.sh
@@ -0,0 +1,35 @@
+# brew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# installs
+# brew install tmux
+# brew install lazygit
+# brew install eza
+# brew install nvim
+# brew install gh
+# brew install ripgrep
+# brew install fd
+# brew install fzf
+# brew install jq
+# brew install git-delta
+# brew install zoxide
+# brew install tree-sitter
+# brew install tree-sitter-cli
+# brew install agent-browser
+# brew install cloc
+# brew install fnm
+# brew install pnpm
+
+# apps
+# brew install --cask raycast
+# brew install --cask rectangle
+# brew install --cask multitouch
+# brew install --cask karabiner-elements
+# brew install --cask app-cleaner
+# brew install --cask ghostty
+# brew install --cask jordanbaird-ice
+
+# ai
+# curl -fsSL https://chatgpt.com/codex/install.sh | sh
+# curl -fsSL https://claude.ai/install.sh | bash
+# curl -fsSL https://opencode.ai/install | bash
